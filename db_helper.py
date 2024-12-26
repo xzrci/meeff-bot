cb301d7e96ac7990cf7a71a0a5dfb076563db27f
xzrci <itxmishii@gmail.com>
2024-12-26 19:48:29 +0500

    Update db_helper.py

--- db_helper.py
+++ db_helper.py
@@ -5,9 +5,13 @@ client = MongoClient("mongodb+srv://itxcriminal:qureshihashmI1@cluster0.jyqy9.mo
 db = client.meeff_tokens
 
 # Insert or update token for a user
-def set_token(user_id, token):
-    db.tokens.update_one({"user_id": user_id, "token": token}, {"$set": {"user_id": user_id, "token": token}}, upsert=True)
-
+def set_token(user_id, token, meeff_user_id):
+    db.tokens.update_one(
+        {"user_id": user_id, "token": token},
+        {"$set": {"user_id": user_id, "token": token, "meeff_user_id": meeff_user_id}},
+        upsert=True
+    )
+    
 # Get all tokens for a user
 def get_tokens(user_id):
     return list(db.tokens.find({"user_id": user_id}, {"_id": 0, "token": 1}))
